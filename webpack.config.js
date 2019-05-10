c17e4153c4f87e318263c441eae8dec482e07386
EanKeen <24364012+EanKeen@users.noreply.github.com>
2019-05-09 23:17:55 -0700

    Add ts integration

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ let CleanWebpackPlugin = require('clean-webpack-plugin');
 let ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
-    mode: 'production',
+    mode: 'development',
     devtool: 'cheap-module-source-map',
     target: 'electron-main',
     context: path.resolve(__dirname),
-    watch: process.env.NODE_ENV === "one" ? false : true,
+    watch: true,
     entry: ['@babel/polyfill', './src/main.js'],
     output: {
         publicPath: './',
@@ -20,22 +20,17 @@ module.exports = {
             '@': './src'
         }
     },
-    plugins: [new CleanWebpackPlugin() /*new ForkTsCheckerWebpackPlugin()*/],
+    plugins: [new CleanWebpackPlugin(), new ForkTsCheckerWebpackPlugin()],
     optimization: {
         minimize: false
     },
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: /\.(js|ts)$/,
                 use: ['babel-loader'],
                 exclude: /node_modules/
             }
-            // {
-            //     test: /\.ts$/,
-            //     use: ['ts-loader'],
-            //     exclude: /node_modules/
-            // }
         ]
     }
 };
