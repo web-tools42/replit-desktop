8ca3ad5a05666e2b83c7c45adcc0aadfdbfb824d
leon332157 <leon332157@gmail.com>
2019-06-23 14:40:26 -0400

    fix: fix code style according to prettier and eslint

--- src/launcher/updater.ts
+++ src/launcher/updater.ts
@@ -1,8 +1,8 @@
-import {app} from 'electron';
+import { app } from 'electron';
 import * as fs from 'fs';
 import * as path from 'path';
 import Axios from 'axios';
-import {decode} from '../lib/run-length-helper';
+import { decode } from '../lib/run-length-helper';
 
 class Updater {
     pathSep: string = path.sep;
@@ -11,7 +11,7 @@ class Updater {
     baseUrl: string = 'http://replit-electron-updater.leon332157.repl.co/';
     userDesktop: string = app.getPath('desktop');
     logFilePath: string = this.userDesktop + 'updater-log.txt';
-    appVersion: string = '0.0.5';//app.getVersion()
+    appVersion: string = '0.0.5'; //app.getVersion()
     logArray: [string] = [''];
 
     constructor() {
@@ -26,50 +26,67 @@ class Updater {
     async check() {
         try {
             const serverResponse = await Axios.get(this.baseUrl + 'check', {
-                params: {'version': this.appVersion},
-                timeout: 10000,
+                params: { version: this.appVersion },
+                timeout: 10000
             });
             if (serverResponse.status !== 200) {
-                throw Error(`Server response code is ${serverResponse.status} instead of 200`);
+                throw Error(
+                    `Server response code is ${serverResponse.status} instead of 200`
+                );
             }
-            this.log(`Successfully requested to sever, response ${serverResponse}`);
+            this.log(
+                `Successfully requested to sever, response ${serverResponse}`
+            );
             const responseObject = JSON.parse(serverResponse.data);
 
-            if (responseObject.hasUpdate || responseObject.version || responseObject.changeLog) {
+            if (
+                responseObject.hasUpdate ||
+                responseObject.version ||
+                responseObject.changeLog
+            ) {
                 return {
                     success: true,
                     hasUpdate: true,
                     version: responseObject.version,
-                    changeLog: responseObject.changeLog,
+                    changeLog: responseObject.changeLog
                 };
             } else {
-                return {success: false, hasUpdate: null, version: null, changeLog: null};
+                return {
+                    success: false,
+                    hasUpdate: null,
+                    version: null,
+                    changeLog: null
+                };
             }
         } catch (e) {
             this.log(`Error: ${e}`);
-            return {success: false, hasUpdate: false};
+            return { success: false, hasUpdate: false };
         }
     }
 
     async download(version: string) {
         try {
-            const serverResponse = await Axios.get(this.baseUrl + `download/${version}`, {timeout: 10000},
+            const serverResponse = await Axios.get(
+                this.baseUrl + `download/${version}`,
+                { timeout: 10000 }
             );
             if (serverResponse.status !== 200) {
-                throw Error(`Server response code is ${serverResponse.status} instead of 200`);
+                throw Error(
+                    `Server response code is ${serverResponse.status} instead of 200`
+                );
             }
             let raw_file = decode(serverResponse.data);
             try {
                 fs.writeFileSync(this.appPath, raw_file, 'base64');
                 this.log('Successfully applied file.');
-                return {success: true};
+                return { success: true };
             } catch (e) {
                 this.log(e);
-                return {success: false};
+                return { success: false };
             }
         } catch (e) {
             this.log(e);
-            return {success: false};
+            return { success: false };
         }
     }
 }
