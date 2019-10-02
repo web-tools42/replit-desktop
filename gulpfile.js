c699e1ae2a7b534b9fd129c44380dc0cd66ac96e
leon332157 <leon332157@gmail.com>
2019-10-02 18:34:51 -0400

    fix: fix gulpfile and remove webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { dest, series, src } = require('gulp');
-const ts = require('gulp-typescript')
-const tsProject  = ts.createProject('tsconfig.json')
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('tsconfig.json');
+
 async function copyFiles() {
     src('package.json').pipe(dest('ts-out'));
 
@@ -10,13 +11,9 @@ async function copyFiles() {
 }
 
 async function build() {
-    const tsResult = src('src/**/*.ts').pipe(tsProject())
-    return tsResult.js.pipe(dest('ts-out/'));
+    src('src/**/*.ts')
+        .pipe(tsProject()).js
+        .pipe(dest('ts-out/'));
 }
 
-function mainTask() {
-    series(build, copyFiles);
-}
-
-exports.default = series(build,copyFiles);
-//exports.default = series(copyFiles, build);
+exports.default = series(build, copyFiles);
