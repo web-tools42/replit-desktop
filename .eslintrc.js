17298425716a5ba845a0b1897cde1d2de54c85c0
leon332157 <leon332157@gmail.com>
2020-05-10 04:14:10 -0400

    chore: fix some dep issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
     env: {
-        "browser": true,
-        "es6": true,
-        "node": true,
-        "commonjs": true
+        browser: true,
+        es6: true,
+        node: true,
+        commonjs: true
     },
     extends: ['prettier', 'plugin:prettier/recommended'],
     parser: '@typescript-eslint/parser',
     plugins: ['eslint-plugin-import', '@typescript-eslint'],
     parserOptions: {
-        "sourceType": "module"
+        sourceType: 'module'
     },
     rules: {
         'prettier/prettier': 'error'
