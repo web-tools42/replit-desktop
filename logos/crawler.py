45c5608c37adcd4d252d9463dcfb056c14fcfba4
frissyn <62220201+frissyn@users.noreply.github.com>
2021-02-05 12:31:32 -0500

    Add VSC Logos (#114)

--- logos/crawler.py
+++ logos/crawler.py
@@ -1,19 +1,24 @@
-# import json
-# from bs4 import BeautifulSoup as bs
-import requests
 import json
 import base64
+import requests
 
-STRING = open('langs.txt','rb').read()
-known = json.loads(base64.b64decode(STRING))
+ses = requests.Session()
+ses.headers.update({
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:64.0) Gecko/20100101 Firefox/64.0 "
+})
+known = ses.get('https://eval.repl.it/languages').json()
 
 for each in known:
-    print(each)
-    link = known[each]['icon']
-    if link.startswith('/'):
-        link = 'https://repl.it'+link
-    content = requests.get(f'{link}', headers={
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:64.0) Gecko/20100101 Firefox/64.0 '}).content
-    #print(content)
-    with open(f'logos-raw/{link.split("/")[-1].replace("?", "")}', 'wb') as f:
-        f.write(content)
+    #print(each)
+    link = each["icon"]
+    link = "https://repl.it" + link if link.startswith("/") else link
+
+    content = ses.get(f"{link}").content
+    # print(content)
+    try:
+        with open(f'logos-raw/{link.split("/")[-1].replace("?", "")}', "wb") as f:
+            f.write(content)
+    except OSError as e:
+        print(e)
+with open('known.json', 'w') as f:
+    json.dump(known, f)
