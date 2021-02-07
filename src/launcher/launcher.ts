ec3f025a428fdecf42f95c59ef119fa4cab019c3
Jake Follest <40705786+spotandjake@users.noreply.github.com>
2021-02-07 16:40:14 -0500

    eslint is the reason i must die

--- src/launcher/launcher.ts
+++ src/launcher/launcher.ts
@@ -59,7 +59,8 @@ class Updater extends EventEmitter {
         const i = Math.floor(Math.log(bytes) / Math.log(k));
 
         return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${
-            sizes[i]}`;
+            sizes[i]
+        }`;
     }
 
     async checkUpdate(): Promise<checkUpdateResult> {
