beae87bad687b22e55d804db02eb87efc1d8e18c
Jake Follest <40705786+spotandjake@users.noreply.github.com>
2021-02-25 10:53:14 -0500

    Added Select All To Ace Keybinds
    Added Select All To Ace Keybinds.

--- src/preload/Ace_Fix.js
+++ src/preload/Ace_Fix.js
@@ -2,6 +2,8 @@ window.onload = () => {
     window.onkeyup = (e) => {
         if (e.ctrlKey && e.keyCode == 86) {
             document.execCommand('paste');
+        } else if (e.ctrlKey && e.keyCode == 65) {
+            document.execCommand('selectAll');
         }
     };
 };
