99ca31ed405a4b689e934fc5965c3b3292efd697
leon332157 <leon332157@gmail.com>
2021-03-10 12:26:13 -0500

    fix double copy

--- src/preload/aceFix.ts
+++ src/preload/aceFix.ts
@@ -1,9 +1,11 @@
 window.onload = () => {
-    window.onkeyup = (e: KeyboardEvent) => {
-        if (e.ctrlKey && e.keyCode == 86) {
-            document.execCommand('paste');
-        } else if (e.ctrlKey && e.keyCode == 65) {
-            document.execCommand('selectAll');
-        }
-    };
+    if (document.getElementsByClassName('ace_editor')) {
+        window.onkeyup = (e: KeyboardEvent) => {
+            if (e.ctrlKey && e.keyCode == 86) {
+                document.execCommand('paste');
+            } else if (e.ctrlKey && e.keyCode == 65) {
+                document.execCommand('selectAll');
+            }
+        };
+    }
 };
