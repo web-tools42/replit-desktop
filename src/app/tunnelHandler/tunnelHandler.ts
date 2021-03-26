43adeab602b1520f03230b2dfb25c81ca4f846fa
leon332157 <leon332157@gmail.com>
2021-03-26 14:38:45 -0400

    wip: implement tunnel connect

--- src/app/tunnelHandler/tunnelHandler.ts
+++ src/app/tunnelHandler/tunnelHandler.ts
@@ -2,7 +2,7 @@ import { ipcMain, IpcMainInvokeEvent } from 'electron';
 import { ElectronWindow } from '../../common';
 import { EventEmitter } from 'events';
 // @ts-ignore
-import { startClient } from 'tcp-over-websockets/client';
+const startClient = require('tcp-over-websockets/client');
 class TunnelHandler extends EventEmitter {
     public tunWindow: ElectronWindow;
     constructor() {
@@ -10,11 +10,18 @@ class TunnelHandler extends EventEmitter {
         ipcMain.handle('tun-connect', this.connectWs);
     }
     connectWs(e: IpcMainInvokeEvent, url: string, remotePort: number, localPort: number) {
-        startClient(url, `127.0.0.1:${remotePort}`, localPort);
+        startClient(url, `127.0.0.1:${remotePort}`, localPort, this.connCb);
     }
     openWindow() {
         this.tunWindow = new ElectronWindow({}, '', true);
         this.tunWindow.loadFile(`${__dirname}/tunnel.html`);
+    }
+    connCb(err) {
+        if (err) {
+            this.sendStatus(`Error: ${err}`);
+        } else {
+            this.sendStatus(`Connected`);
+        }
     }
     sendStatus(status: string) {
         ipcMain.emit('tun-status-update', status);
