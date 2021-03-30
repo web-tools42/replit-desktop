08224a89001304afd02a1ac5195c242de57b28d7
leon332157 <leon332157@gmail.com>
2021-03-29 23:57:12 -0400

    add disconnect (wip)

--- src/app/tunnelHandler/tunnelHandler.ts
+++ src/app/tunnelHandler/tunnelHandler.ts
@@ -1,27 +1,39 @@
 import { ipcMain, IpcMainInvokeEvent } from 'electron';
 import { ElectronWindow } from '../../common';
 import { EventEmitter } from 'events';
+import fetch from 'node-fetch';
 // @ts-ignore
 const startClient = require('tcp-over-websockets/client');
 class TunnelHandler {
     public tunWindow: ElectronWindow;
+    private conn: any;
     constructor() {
         ipcMain.handle('tun-connect', this.connectWs.bind(this));
+        ipcMain.handle('tun-disconnect', this.disconnectWs.bind(this));
     }
     connectWs(e: IpcMainInvokeEvent, url: string, remotePort: number, localPort: number) {
-        startClient(url, `127.0.0.1:${remotePort}`, localPort, this.connCb.bind(this));
+        if (this.conn) return;
+        fetch(url)
+            .then(() => {
+                this.conn = startClient(url, `127.0.0.1:${remotePort}`, localPort, this.connCb.bind(this));
+            })
+            .catch((err) => {
+                this.sendStatus(err.toString());
+            });
     }
+    disconnectWs(e: IpcMainInvokeEvent) {}
     openWindow() {
         if (!this.tunWindow) this.tunWindow = new ElectronWindow({}, '', true, true);
         this.tunWindow.loadFile(`${__dirname}/tunnel.html`);
         this.tunWindow.once('close', () => {
-            delete this.tunWindow;
+            this.tunWindow.destroy();
+            this.conn.destroy();
         });
     }
     connCb(err: any) {
-        console.log(err);
         if (err) {
             this.sendStatus(`Error: ${err}`);
+            this.conn.destroy();
         } else {
             this.sendStatus(`Connected`);
         }
